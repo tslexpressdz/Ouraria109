e349864ac346d1bb9bbac50055efdf9abb7018bc
tslexpressdz <tslexpressdz@gmail.com>
2026-08-22 14:31:40 +0100

    Update formatters.dart

--- lib/utils/formatters.dart
+++ lib/utils/formatters.dart
@@ -1,1 +1,14 @@
+import 'package:intl/intl.dart';
 
+/// تنسيق الأرقام كعملة (مثال: 1,250 دج)
+final _currencyFormat = NumberFormat.decimalPattern();
+
+String formatCurrency(double value) {
+  return '${_currencyFormat.format(value)} دج';
+}
+
+final _dateFormat = DateFormat('yyyy/MM/dd - HH:mm');
+
+String formatDate(DateTime date) {
+  return _dateFormat.format(date);
+}
